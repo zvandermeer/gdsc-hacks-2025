693f492676da4e447be361245917177c54b63bc9
cssyed <syedmarhuss@gmail.com>
2025-05-03 16:31:19 -0400

    added component for all new pages

--- web-app/src/components/NewPage.jsx
+++ web-app/src/components/NewPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import close from '../assets/close.png';
 
+// for all new pages like settings and profile
 const PopUpWrapper = () => {
 
   return (
