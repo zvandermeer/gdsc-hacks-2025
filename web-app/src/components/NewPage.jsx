138492f6e2e5685ca684a272c3dcdc5f1087dbf5
cssyed <syedmarhuss@gmail.com>
2025-05-03 16:25:00 -0400

    setting up new pages for settings & profile

---
+++ web-app/src/components/NewPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+import close from '../assets/close.png';
+
+const PopUpWrapper = () => {
+
+  return (
+
+    <div>PopUpWrapper</div>
+
+  )
+  
+}
+
+export default PopUpWrapper
