57af5f4ac9954262172d216cc89db5c7e9e19a39
Zoey VanderMeer <zoeyvanderm@gmail.com>
2025-05-03 14:01:52 -0400

    Fix notification breaking with more than one device

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pywebpush import webpush, WebPushException
@@ -18,19 +19,26 @@ load_dotenv()
 
 VAPID_PUBLIC_KEY = os.getenv("VAPID_PUBLIC")
 VAPID_PRIVATE_KEY = os.getenv("VAPID_PRIVATE")
-VAPID_CLAIMS = {"sub": "mailto:zoeyvanderm@gmail.com"}
 
 # Scheduler
 scheduler = BackgroundScheduler()
 scheduler.start()
 
 def send_push(subscription_info, data):
+    endpoint = subscription_info['endpoint']
+    audience = f"{urlparse(endpoint).scheme}://{urlparse(endpoint).netloc}"
+    
+    vapid_claims = {
+        "sub": "mailto:your@email.com",
+        "aud": audience
+    }
+
     try:
         webpush(
             subscription_info,
             data=json.dumps(data),
             vapid_private_key=VAPID_PRIVATE_KEY,
-            vapid_claims=VAPID_CLAIMS
+            vapid_claims=vapid_claims
         )
         print("Notification sent.")
     except WebPushException as ex:
@@ -60,4 +68,4 @@ def schedule_push():
     return jsonify({"status": "scheduled", "run_at": run_time.isoformat()}), 200
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5151)
